5a27d609ba684f43a99ab5d0794f0ce9dc926089
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-25 11:39:27 +0400

    change DB_URI

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 // Load environment variables from .env
 require('dotenv').config();
 
-let DB_URI = `postgresql://`;
+let DB_URI = process.env.DATABASE_URL;
 
 if (process.env.NODE_ENV === 'test') {
     DB_URI = `${DB_URI}/books-test`;
 } else {
-    DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+    DB_URI = `${DB_URI}/books`;
 }
 
 module.exports = { DB_URI };
