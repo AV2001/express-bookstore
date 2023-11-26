82a2a489da3a42b2d06218185192cf39eb6d8bee
AV2001 <anirudhvadlamani2001@gmail.com>
2023-11-26 19:32:47 +0400

    add tests for POST /books

--- routes/book.test.js
+++ routes/book.test.js
@@ -45,3 +45,27 @@ describe('GET /books/:id', () => {
         expect(response.statusCode).toBe(404);
     });
 });
+
+describe('POST /books', () => {
+    test('Create a book', async () => {
+        const newBook = {
+            isbn: '978-0-948712-76-0',
+            amazon_url: 'https://www.richardson.com/',
+            author: 'Dana Smith',
+            language: 'byn',
+            pages: 987,
+            publisher: 'Cooper, Snyder and Garza',
+            title: 'Adaptive foreground model',
+            year: 1960,
+        };
+        const response = await request(app).post('/books').send(newBook);
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual({ book: newBook });
+    });
+
+    test('Return 400 if missing properties are passed', async () => {
+        const newBook = {};
+        const response = await request(app).post('/books').send(newBook);
+        expect(response.statusCode).toBe(400);
+    });
+});
